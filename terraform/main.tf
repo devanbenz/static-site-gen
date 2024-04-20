d6669a658ce4cc03c158301b2d9f9718281f59bf
Devan <devandbenz@gmail.com>
2024-04-20 16:38:56 -0500

    default root

--- terraform/main.tf
+++ terraform/main.tf
@@ -166,8 +166,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   price_class = "PriceClass_100"
 
   ordered_cache_behavior {
-    path_pattern     = "blog/*"
-    target_origin_id = "whateverforever.computer"
+    path_pattern        = "blog/*"
+    target_origin_id    = "whateverforever.computer"
+    default_root_object = "index.html"
 
     forwarded_values {
       query_string = false
