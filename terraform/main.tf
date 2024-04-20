bab5a488cb736e8d2636e55145dd389bc6074cc7
Devan <devandbenz@gmail.com>
2024-04-20 16:09:00 -0500

    adds CDN and acm for route53

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,3 +78,106 @@ resource "aws_s3_bucket_website_configuration" "whatever_forever_site" {
     suffix = "index.html"
   }
 }
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "whateverforever.computer"
+  validation_method = "DNS"
+
+  tags = {
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+data "aws_route53_zone" "primary" {
+  name = "whateverforever.computer."
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.whatever_forever.website_endpoint
+    origin_id   = "whateverforever.computer"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "S3 Hosting Distribution"
+  default_root_object = "index.html"
+
+  aliases = ["example.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "whateverforever.computer"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2019"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  price_class = "PriceClass_100"
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for ${aws_s3_bucket.whatever_forever.id}"
+}
+
+resource "aws_route53_record" "cdn" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "whateverforever.computer"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
