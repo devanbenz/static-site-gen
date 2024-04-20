daf1bec536181490952da523caf184f9c4c51052
Devan <devandbenz@gmail.com>
2024-04-20 16:32:43 -0500

    fix cloudfront

--- terraform/main.tf
+++ terraform/main.tf
@@ -164,6 +164,25 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   price_class = "PriceClass_100"
+
+  ordered_cache_behavior {
+    path_pattern     = "blog/*"
+    target_origin_id = "whateverforever.computer"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+  }
 }
 
 resource "aws_cloudfront_origin_access_identity" "oai" {
